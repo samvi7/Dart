e1ebf5d42f68c1727de33f3e5cce6f21e3e9c1f8
samvi7 <isc.samuel.vi@gmail.com>
2024-11-12 20:17:27 -0600

    Update 02_variables.dart

--- 02_variables.dart
+++ 02_variables.dart
@@ -1,1 +1,18 @@
-
+void main(){
+  
+  final String hero = "Spider Man";
+  final int rank = 80;
+  final bool isAlive = true;
+  final List<String> abilities = ['jump'];
+  final gallery = <String>['spider/front.png', 'spider/back.png'];
+  
+  print("""
+  
+    $hero
+    $rank
+    $isAlive
+    $gallery
+    $abilities
+  
+  """);
+}
